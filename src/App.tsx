ea1d95f6a3283252051f8c55409293801e690c00
Katchie <41813266+Ka-Da-Sci@users.noreply.github.com>
2025-02-20 13:01:42 +0100

    Add and setup firebase.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import { Route, Routes } from "react-router-dom";
 import HomePage from "./components/pages/home/Index";
 import './styles/globals.css'
+import _firebaseApp from "./lib/firebase.config";
+import { getAnalytics } from "firebase/analytics";
 
+const firebaseApp = _firebaseApp();
+export const analytics = getAnalytics(firebaseApp);
 
 const App =() => {
   return (
---
+++ src/lib/firebase.config.ts
@@ -0,0 +1,28 @@
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// The web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_iD,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+};
+
+// Initialize Firebase
+/* eslint-disable no-console */
+const app = () => {
+    if (!firebaseConfig || !firebaseConfig.apiKey) {
+      console.log("Firebase Config:", firebaseConfig);
+        throw new Error('No firebase configuration provided.');
+    } else {
+      console.log('Firebase Initialized');
+    }
+    return initializeApp(firebaseConfig)
+};
+
+export default app;
